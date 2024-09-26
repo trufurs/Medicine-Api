7081a4ad0e073ee9f98d9b722968e6e9a939d75d
Isht007 <118360672+Isht007@users.noreply.github.com>
2024-09-26 10:31:14 +0530

    Added some filerting to apiService

--- services/apiService.js
+++ services/apiService.js
@@ -1,38 +1,57 @@
-const axios = require('axios');
-const dotenv = require('dotenv');
+const axios = require("axios");
+const dotenv = require("dotenv");
 dotenv.config();
 
 const fetchMedicineFromOpenFDA = async (query) => {
-  const url = `https://api.fda.gov/drug/label.json?search=${query}&limit=1`;
-  try {
-    const response = await axios.get(url);
-    return response.data.results;
-  } catch (error) {
-    console.error('Error fetching data from OpenFDA:', error.message);
-    return null;
-  }
+	const url = `https://api.fda.gov/drug/label.json?search=${query}&limit=5`;
+	try {
+		const medicines = await axios.get(url);
+		if (medicines.length > 0) {
+			// Extract essential information from the response
+			const results = medicines.map((item) => ({
+				brand_name: item.openfda.brand_name
+					? item.openfda.brand_name[0]
+					: "N/A",
+				warnings: item.warnings ? item.warnings[0] : "N/A",
+				usage: item.indications_and_usage
+					? item.indications_and_usage[0]
+					: "N/A",
+				precautions: item.precautions ? item.precautions[0] : "N/A",
+				dosage_and_administration: item.dosage_and_administration
+					? item.dosage_and_administration[0]
+					: "N/A",
+			}));
+
+			return res.send(results);
+		}
+	} catch (error) {
+		console.error("Error fetching data from OpenFDA:", error.message);
+		return null;
+	}
 };
 
 const fetchMedicineFromMedlinePlusAPI = async (query) => {
-  const url = `https://wsearch.nlm.nih.gov/ws/query?db=healthTopics&term=${query}`;
-  try {
-    const response = await axios.get(url);
-    return response.data;
-  } catch (error) {
-    console.error('Error fetching data from Another API:', error.message);
-    return null;
-  }
+	const url = `https://wsearch.nlm.nih.gov/ws/query?db=healthTopics&term=${query}`;
+	try {
+		const response = await axios.get(url);
+		return response.data;
+	} catch (error) {
+		console.error("Error fetching data from Another API:", error.message);
+		return null;
+	}
 };
 
 // Aggregate data from multiple APIs
 const fetchAggregatedMedicineData = async (medicineName) => {
-  const openFDAData = await fetchMedicineFromOpenFDA(medicineName);
-  const medlinePlusAPIData = await fetchMedicineFromMedlinePlusAPI(medicineName);
-  
-  return {
-    openFDA: openFDAData,
-    medlinePlusAPI: medlinePlusAPIData
-  };
+	const openFDAData = await fetchMedicineFromOpenFDA(medicineName);
+	const medlinePlusAPIData = await fetchMedicineFromMedlinePlusAPI(
+		medicineName
+	);
+
+	return {
+		openFDA: openFDAData,
+		medlinePlusAPI: medlinePlusAPIData,
+	};
 };
 
 module.exports = { fetchAggregatedMedicineData };
